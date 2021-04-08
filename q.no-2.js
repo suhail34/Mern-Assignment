d1f5b6d539d7d4e4c46bf634aa0f223fbd755620
suhail34 <77241166+suhail34@users.noreply.github.com>
2021-04-08 21:18:35 +0530

    Delete q.no-2.js

--- q.no-2.js
+++
@@ -1,7 +0,0 @@
-var readline = require('readline-sync');
-var amount = readline.question('Enter the amount : ');
-var rate = readline.question('Enter the rate : ');
-var time = readline.question('Enter the time : ');
-
-var SimpleInterest = (amount*rate*time)/100;
-console.log("The simple interest is : "+SimpleInterest);
